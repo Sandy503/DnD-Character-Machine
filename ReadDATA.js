02bc1a3d4fe3d9a99201660a6251cb9fa7ff1880
Spencer Purcell <spencerpurcell@Spencers-MacBook-Pro.local>
2020-02-08 12:36:27 -0800

    updated readDATA

--- ReadDATA.js
+++ ReadDATA.js
@@ -18,14 +18,26 @@ connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
   showMods();
+  showClasses();
 });
 
 function showMods() {
   connection.query("SELECT * FROM race", function(err, res) {
     if (err) throw err;
     for (var i = 0; i < res.length; i++) {
-      console.log(res[i].id + " | " + res[i].STR_mod + " | " + res[i].DEX_mod + " | " + res[i].CONST_mod + " | " + res[i].INT_mod + " | ");
+      console.log(res[i].id + " | " + res[i].race + " | " + res[i].STR_mod + " | " + res[i].DEX_mod + " | " + res[i].CONST_mod + " | " + res[i].INT_mod + " | ");
     }
     console.log("-----------------------------------"); 
+  });
+}
+
+function showClasses() {
+  connection.query("SELECT * FROM class", function(err, res) {
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+      console.log(res[i].id + " | " + res[i].Class + " | " + res[i].Hit_die + " | " + res[i].Primary_ability);
+    }
+    console.log("-----------------------------------"); 
+    connection.end();
   });
 }
