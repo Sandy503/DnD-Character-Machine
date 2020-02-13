ace41cfae33338b38ee117aab54dbd47759e051a
Kyle <kdsandoval2@gmail.com>
2020-02-12 18:46:12 -0800

    Added models for class and race

---
+++ models/class.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, Datatypes) => {
+    const Class = sequelize.define("Class", {
+        class: {
+            type: Datatypes.STRING,
+            allowNull: false
+        },
+        hit_die: {
+            type: Datatypes.STRING
+        },
+        primary_ability: {
+            type: Datatypes.String
+        }
+    });
+    return Class
+}
